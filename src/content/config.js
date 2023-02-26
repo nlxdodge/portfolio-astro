c6600a1b0bec77869889baa4bb0d25da5af64a42
NLxDoDge <nlxdodge@gmail.com>
2023-02-26 15:16:01 +0100

    chore: code formatting

--- src/content/config.js
+++ src/content/config.js
@@ -1,30 +1,30 @@
 import { defineCollection, z } from 'astro:content';
 
 const worksCollection = defineCollection({
-    schema: z.object({
-        draft: z.boolean().optional(),
-        title: z.string(),
-        order: z.number(),
-        author: z.string(),
-        date: z.date(),
-        image: z.string(),
-        description: z.string(),
-        keywords: z.array(z.string()).optional(),
-        category: z.string()
-    })
+  schema: z.object({
+    draft: z.boolean().optional(),
+    title: z.string(),
+    order: z.number(),
+    author: z.string(),
+    date: z.date(),
+    image: z.string(),
+    description: z.string(),
+    keywords: z.array(z.string()).optional(),
+    category: z.string()
+  })
 });
 const postsCollection = defineCollection({
-    schema: z.object({
-        draft: z.boolean().optional(),
-        title: z.string(),
-        author: z.string(),
-        date: z.date(),
-        description: z.string(),
-        keywords: z.array(z.string()),
-    })
+  schema: z.object({
+    draft: z.boolean().optional(),
+    title: z.string(),
+    author: z.string(),
+    date: z.date(),
+    description: z.string(),
+    keywords: z.array(z.string()),
+  })
 });
 
 export const collections = {
-    'works': worksCollection,
-    'posts': postsCollection,
+  'works': worksCollection,
+  'posts': postsCollection,
 };
