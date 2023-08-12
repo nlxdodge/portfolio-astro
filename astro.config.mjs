0931fe3d5ae769cf0e0030c75e64a9118ae89a66
NLxDoDge <nlxdodge@gmail.com>
2023-08-12 14:27:30 +0200

    fix: images not being displayed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import robotsTxt from "astro-robots-txt";
-
-// https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 import compress from "astro-compress";
@@ -18,10 +15,16 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerbenveenhof.nl/',
-  integrations: [sitemap(), robotsTxt(), image(), compress(), mdx()],
+  integrations: [sitemap(), robotsTxt(), compress(), mdx()],
   vite: {
     ssr: {
       noExternal: ['include-media']
     }
-  }
+  },
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService(),
+  },
 });
