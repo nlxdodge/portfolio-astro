639b63ae5429f92ba457c6b4007f9fb24bdaf14b
NLxDoDge <nlxdodge@gmail.com>
2023-08-22 17:16:59 +0200

    feat: move away from scss globals and add csspurge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,27 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 
-// https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
-import robotsTxt from "astro-robots-txt";
-
-// https://astro.build/config
-import compress from "astro-compress";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import compress from "astro-compress";
+import purgecss from 'astro-purgecss';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gerbenveenhof.nl/',
-  integrations: [sitemap(), robotsTxt(), compress(), mdx()],
+  integrations: [sitemap(), robotsTxt(), compress(), mdx(), purgecss()],
   vite: {
     ssr: {
       noExternal: ['include-media']
-    }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // this is required so that we don't need to import the globals every time
+                additionalData: '@use "/src/styles/globals.scss" as *;',
+            },
+        },
+    },
   },
   experimental: {
     assets: true
