90c46e46bb2ebbbf5b8e5759ffa4ce0507487274
NLxDoDge <nlxdodge@gmail.com>
2024-07-15 10:30:23 +0200

    feat: add prettier formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,57 +1,57 @@
-import { defineConfig, sharpImageService } from 'astro/config';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import { defineConfig, sharpImageService } from "astro/config";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import partytown from "@astrojs/partytown";
-import remarkToc from 'remark-toc';
+import remarkToc from "remark-toc";
 import purgecss from "astro-purgecss";
 import icon from "astro-icon";
 import compress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gerbenveenhof.nl/',
+  site: "https://gerbenveenhof.nl/",
   integrations: [
     icon(),
     sitemap(),
     robotsTxt(),
     mdx({
       shikiConfig: {
-        theme: 'one-dark-pro',
+        theme: "one-dark-pro",
         themes: {
           light: "one-light",
-          dark: "one-dark-pro"
-        }
+          dark: "one-dark-pro",
+        },
       },
     }),
     partytown(),
     purgecss(),
-    compress()
+    compress(),
   ],
   vite: {
     ssr: {
-      noExternal: ['include-media']
+      noExternal: ["include-media"],
     },
     css: {
       preprocessorOptions: {
         scss: {
           // this is required so that we don't need to import the globals every time
-          additionalData: '@use "/src/styles/globals.scss" as *;'
-        }
-      }
-    }
+          additionalData: '@use "/src/styles/globals.scss" as *;',
+        },
+      },
+    },
   },
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
   markdown: {
     remarkPlugins: [remarkToc],
     rehypePlugins: [rehypeAccessibleEmojis],
   },
   redirects: {
-    '/posts': '/posts/1',
-    '/post': '/posts/1',
-    '/work': '/works'
-  }
+    "/posts": "/posts/1",
+    "/post": "/posts/1",
+    "/work": "/works",
+  },
 });
--- src/content/config.js
+++ src/content/config.js
@@ -1,18 +1,19 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const worksCollection = defineCollection({
-  schema: ({ image }) => z.object({
-    draft: z.boolean().optional(),
-    title: z.string(),
-    order: z.number(),
-    author: z.string(),
-    date: z.date(),
-    image: image(),
-    image_alt: z.string(),
-    description: z.string(),
-    tags: z.array(z.string()).optional(),
-    category: z.string()
-  })
+  schema: ({ image }) =>
+    z.object({
+      draft: z.boolean().optional(),
+      title: z.string(),
+      order: z.number(),
+      author: z.string(),
+      date: z.date(),
+      image: image(),
+      image_alt: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).optional(),
+      category: z.string(),
+    }),
 });
 const postsCollection = defineCollection({
   schema: z.object({
@@ -22,10 +23,10 @@ const postsCollection = defineCollection({
     date: z.date(),
     description: z.string(),
     tags: z.array(z.string()),
-  })
+  }),
 });
 
 export const collections = {
-  'works': worksCollection,
-  'posts': postsCollection,
+  works: worksCollection,
+  posts: postsCollection,
 };
