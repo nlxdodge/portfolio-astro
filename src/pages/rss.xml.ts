68e930dbca0d3786857f56c22d7b2e5b7ea79dd9
NLxDoDge <nlxdodge@gmail.com>
2025-08-22 14:07:30 +0200

    feat: migrate RSS page to typescript and fix it as well

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,21 +1,27 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import type { APIRoute } from 'astro';
 
 const posts = await getCollection("posts", ({ data }) => {
   return data.draft !== true;
-});
+})
 
-export function get(context) {
+const sortedPosts = posts.sort((first, second) =>
+  first.data.date < second.data.date ? 1 : -1
+)
+
+export const GET: APIRoute = async ({ site }) => {
   return rss({
     title: "Gerben Veenhof",
     description: "My programming and coding odyssey",
-    site: context.site,
-    items: posts.map((post) => ({
+    site: site,
+    items: sortedPosts.map((post) => ({
       title: post.data.title,
+      link: `/post/${post.slug}/`,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/post/${post.slug}/`,
     })),
     customData: `<language>en-us</language>`,
+    
   });
 }
